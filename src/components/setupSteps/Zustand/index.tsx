1ddeb946905a32694812b86eb01e7431c4fa931d
Ariel <thepiratemermaid4@gmail.com>
2025-03-21 19:40:55 -0600

    add zustand steps

--- src/components/setupSteps/Zustand/index.tsx
+++ src/components/setupSteps/Zustand/index.tsx
@@ -1,5 +1,53 @@
-import { Box } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { CodeBlock } from '~/components';
 
 export const ZustandSetupStep = () => {
-  return <Box>ZustandSetupStep TODO</Box>;
+  return (
+    <Stack spacing={2}>
+      <Box>
+        <Typography>1. Install</Typography>
+        <CodeBlock>npm install zustand</CodeBlock>
+      </Box>
+
+      <Box>
+        <Typography>2. Set up Store</Typography>
+        <Typography>
+          Example with some values persisted in local storage:
+        </Typography>
+        <CodeBlock multiline>{`
+    import { create } from 'zustand';
+    import { persist } from 'zustand/middleware';
+
+    type State = {
+      darkMode: boolean;
+      toggleDarkMode: () => void;
+    };
+
+    export const useStore = create<State, [['zustand/persist', Partial<State>]]>(
+      persist(
+        (set, get) => ({
+          darkMode: false,
+          toggleDarkMode: () => {
+            const darkMode = get().darkMode;
+            return set({ darkMode: !darkMode });
+          }
+        }),
+        {
+          name: 'userSettings',
+          partialize: (state: State) => ({
+            darkMode: state.darkMode
+          })
+        }
+      )
+    );
+        `}</CodeBlock>
+      </Box>
+
+      <Box>
+        <Typography>3. Use store values in components</Typography>
+        <CodeBlock>{`const { darkMode } = useStore((state) => state.darkMode);`}</CodeBlock>
+      </Box>
+    </Stack>
+  );
 };

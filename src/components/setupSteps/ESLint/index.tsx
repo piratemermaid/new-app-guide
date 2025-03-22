e64147473e2aadabb296eb3c74ab9627d5396599
Ariel <thepiratemermaid4@gmail.com>
2025-03-21 20:08:53 -0600

    add eslint steps

--- src/components/setupSteps/ESLint/index.tsx
+++ src/components/setupSteps/ESLint/index.tsx
@@ -1,5 +1,36 @@
-import { Box } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { CodeBlock, ExampleTitle, StepTitle } from '~/components';
 
 export const ESLintSetupStep = () => {
-  return <Box>ESLintSetupStep TODO</Box>;
+  return (
+    <Stack spacing={2}>
+      <Box>
+        <StepTitle>1. Install and configure</StepTitle>
+        <CodeBlock>npm install --save-dev eslint @eslint/js</CodeBlock>
+        <Typography variant="body2">To configure:</Typography>
+        <CodeBlock>npx eslint --init</CodeBlock>
+        <Typography variant="body2">To run:</Typography>
+        <CodeBlock>npx eslint yourfile.js</CodeBlock>
+        <ExampleTitle>Example: Adding rules/settings</ExampleTitle>
+        <CodeBlock>{`
+  export default [
+    ...initialConfig,
+    {
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    },
+    {
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
+  ];
+  `}</CodeBlock>
+      </Box>
+    </Stack>
+  );
 };
